523d42db846879548ee918e27d84b9c47594396c
JTew27 <145987450+JTew27@users.noreply.github.com>
2023-10-01 20:18:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+group1 = 113
+group2= 175
+group3= 12
+
+split1 = round(group1/24)
+rem1= (group1 % 24)
+print ("There is",split1, "groups for the first set with a small group of", rem1)
+print("\n")
+
+split2 = round(group2/24)
+rem2= (group2 % 24)
+print ("There is",split2, "groups for the second set with a small group of", rem2)
+print("\n")
+
+split3 = round(group3/24)
+rem3= (group3 % 24)
+print ("There is",split3, "groups for the third set with a small group of", rem3)
+print("\n")
